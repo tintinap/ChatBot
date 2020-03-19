7efc5d9c79921ee24e19f61dada94a8e23194d6b
tintinap <31465850+tintinap@users.noreply.github.com>
2020-03-19 22:30:30 +0700

    training

--- main.py
+++ main.py
@@ -21,14 +21,14 @@ for intent in data['intents']:
     for pattern in intent['patterns']:
         wrds = nltk.word_tokenize(pattern)
         words.extend(wrds)
-        docs_x.append(pattern)
+        docs_x.append(wrds)
         docs_y.append(intent['tag'])
 
 
     if intent['tag'] not in labels:
         labels.append(intent['tag'])
 
-words = [stemmer.stem(w.lower()) for w in words]
+words = [stemmer.stem(w.lower()) for w in words if w not in "?"]
 words = sorted(list(set(words)))
 
 labels = sorted(labels)
@@ -54,5 +54,18 @@ for x, doc in enumerate(docs_x):
     training.append(bag)
     output.append(output_row)
 
-traning = np.array(training)
+training = np.array(training)
 output = np.array(output)
+
+tf.reset_default_graph()
+
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+
+model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+model.save("model.tflearn")
