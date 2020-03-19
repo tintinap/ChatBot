ec3e6a3400040da7a0e68689c157cba63fcdd4ef
tintinap <31465850+tintinap@users.noreply.github.com>
2020-03-19 20:41:45 +0700

    prepare data

--- main.py
+++ main.py
@@ -3,25 +3,56 @@ from nltk.stem.lancaster import LancasterStemmer
 
 stemmer = LancasterStemmer()
 
-import numpy
+import numpy as np
 import tflearn
 import tensorflow as tf
 import random
 import json
 
-with open(".\json file\intents.json") as file:
+with open(r".\json file\intents.json") as file:
     data = json.load(file)
     
 words = []
 labels = []
-docs = []
+docs_x = [] #list of all different pattern
+docs_y = [] #words and tag
 
 for intent in data['intents']:
     for pattern in intent['patterns']:
         wrds = nltk.word_tokenize(pattern)
         words.extend(wrds)
-        docs.append(pattern)
+        docs_x.append(pattern)
+        docs_y.append(intent['tag'])
 
 
     if intent['tag'] not in labels:
         labels.append(intent['tag'])
+
+words = [stemmer.stem(w.lower()) for w in words]
+words = sorted(list(set(words)))
+
+labels = sorted(labels)
+
+training = []
+output = []
+
+out_empyty =  out_empty=[0 for _ in range(len(labels))]
+
+for x, doc in enumerate(docs_x):
+    bag = [] # to see that each word is exist or not
+
+    wrds = [stemmer.stem(w) for w in doc]
+
+    for w in words:
+        if w in wrds:
+            bag.append(1)
+        else:
+            bag.append(0)
+    output_row = out_empyty[:]
+    output_row[labels.index(docs_y[x])] = 1
+
+    training.append(bag)
+    output.append(output_row)
+
+traning = np.array(training)
+output = np.array(output)
